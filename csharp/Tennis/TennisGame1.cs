6f13e352534162a641c6d15286975d69d1566772
FrederikKongstedMadsen <frederik.51@live.dk>
2023-03-27 13:45:43 +0200

    Usec Extract Method Refactoring

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -21,60 +21,83 @@ namespace Tennis
 
         public string GetScore()
         {
-            string score = "";
-            var tempScore = 0;
-            if (_player1.MScore == _player2.MScore)
+			string score = "";
+			if (_player1.MScore == _player2.MScore)
             {
-                switch (_player1.MScore)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
-                }
+	            score = GetEvenScore();
             }
             else if (_player1.MScore >= 4 || _player2.MScore >= 4)
             {
-                var minusResult = _player1.MScore - _player2.MScore;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+	            score = GetWinnerOrAdvantage();
+
             }
             else
             {
-                for (var i = 1; i < 3; i++)
-                {
-                    if (i == 1) tempScore = _player1.MScore;
-                    else { score += "-"; tempScore = _player2.MScore; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
-                }
+	            score = GetNormalScore();
             }
-            return score;
+
+			return score;
         }
+
+        private string GetEvenScore()
+        {
+	        string score = "";
+			switch (_player1.MScore)
+	        {
+		        case 0:
+			        score = "Love-All";
+			        break;
+		        case 1:
+			        score = "Fifteen-All";
+			        break;
+		        case 2:
+			        score = "Thirty-All";
+			        break;
+		        default:
+			        score = "Deuce";
+			        break;
+
+	        }
+            return score;
+		}
+
+        private string GetWinnerOrAdvantage()
+        {
+	        string score = "";
+			var minusResult = _player1.MScore - _player2.MScore;
+	        if (minusResult == 1) score = "Advantage player1";
+	        else if (minusResult == -1) score = "Advantage player2";
+	        else if (minusResult >= 2) score = "Win for player1";
+	        else score = "Win for player2";
+            return score;
+		}
+
+        private string GetNormalScore()
+        {
+	        string score = "";
+	        var tempScore = 0;
+			for (var i = 1; i < 3; i++)
+	        {
+		        if (i == 1) tempScore = _player1.MScore;
+		        else { score += "-"; tempScore = _player2.MScore; }
+		        switch (tempScore)
+		        {
+			        case 0:
+				        score += "Love";
+				        break;
+			        case 1:
+				        score += "Fifteen";
+				        break;
+			        case 2:
+				        score += "Thirty";
+				        break;
+			        case 3:
+				        score += "Forty";
+				        break;
+		        }
+	        }
+			return score;
+		}
     }
 }
 
